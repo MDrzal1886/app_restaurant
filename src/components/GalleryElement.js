e968ba68eb9cb3ae45648b84297f3f4311eb4362
Mateusz Drzał <mdrzal1886@gmail.com>
2021-10-24 18:57:05 +0200

    add media to gallery

--- src/components/GalleryElement.js
+++ src/components/GalleryElement.js
@@ -1,6 +1,6 @@
 const GalleryElement = (image) => {
   return (
-    <a href={image.src} key={image.id} className="text-center p-1">
+    <a href={image.src} key={image.id} className="text-center py-4">
       <img src={image.thumbnail} alt={image.description} />
     </a>
   );
--- src/pages/GalleryPage.js
+++ src/pages/GalleryPage.js
@@ -3,20 +3,22 @@ import GalleryElement from "../components/GalleryElement";
 
 import { gallery } from "../data";
 
+import "../sass/galleryPage.scss";
+
 const GalleryPage = () => {
   const galleryElements = gallery.map((image) => (
     <GalleryElement key={image.id} {...image} />
   ));
 
   return (
-    <>
+    <div className="galleryContainer">
       <h1 className="text-center p-3 titleFontFamyli">Galeria</h1>
       <SRLWrapper>
-        <div className="d-flex flex-column justify-content-between">
+        <div className="d-flex flex-column flex-sm-row justify-content-around flex-wrap">
           {galleryElements}
         </div>
       </SRLWrapper>
-    </>
+    </div>
   );
 };
 
